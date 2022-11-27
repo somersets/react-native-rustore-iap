e94bf2b9e79da3aeb94410c828b04965264dd2cd
Nikita Likhachev <facelessmay@gmail.com>
2022-11-27 22:44:21 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  RustoreIapExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-rustore-iap': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
