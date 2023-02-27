85f8b01f099d6e7bb532f5818990728d8dcb9f13
Nikita Likhachev <facelessmay@gmail.com>
2023-02-27 13:57:03 +0700

    fix: revert ios folder

--- ios/RustoreIap.swift
+++ ios/RustoreIap.swift
@@ -1,6 +1,6 @@
 @objc(RustoreIap)
 class RustoreIap: NSObject {
-
+// stub
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
